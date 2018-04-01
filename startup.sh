00fc37105ab2181b851c1452a55528fda9133259
Malte Poll <malte.poll@web.de>
2018-04-01 23:30:44 +0200

    Listen on any

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 openbox-session&
-x11vnc -display :1 -passwd ${VNC_PASSWD} -listen localhost -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -passwd ${VNC_PASSWD} -xkb -ncache 10 -ncache_cr -forever &
