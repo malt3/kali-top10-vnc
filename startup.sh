65732b02e5ff0ba2405a5eccef81c56bb495c61a
Malte Poll <malte.poll@web.de>
2018-04-02 00:36:33 +0200

    changed vnc settings

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export DISPLAY=:1
-Xvfb :1 -screen 0 1600x900x16 &
+Xvfb :1 -screen 0 1920x1080x24 &
 sleep 5
 startxfce4&
-x11vnc -display :1 -passwd ${VNC_PASSWD} -xkb -ncache 10 -ncache_cr -forever &
+# x11vnc -display :1 -passwd ${VNC_PASSWD} -xkb -ncache 10 -ncache_cr -forever &
+x11vnc -display :1 -passwd ${VNC_PASSWD} -xkb -forever &
