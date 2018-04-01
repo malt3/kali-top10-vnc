dfed6e24306a4a60c4ee42bbc0728c472ff642c3
Malte Poll <malte.poll@web.de>
2018-04-02 00:42:50 +0200

    changed resolution

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DISPLAY=:1
-Xvfb :1 -screen 0 1920x1080x24 &
+Xvfb :1 -screen 0 2224x1668x24 &
 sleep 5
 startxfce4&
 # x11vnc -display :1 -passwd ${VNC_PASSWD} -xkb -ncache 10 -ncache_cr -forever &
